b26c9316ad3165a10ead48893656470e7035a14a
damoncrockett <damoncrockett@hotmail.com>
2022-07-19 12:09:49 -0400

    scrolling centered table box

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,55 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from 'react';
+import { select } from 'd3-selection';
 
-import '../assets/css/style.css';
+const row = new Array(5).fill('material data cell');
+const arr = new Array(1000).fill(0);
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-                <h1>My React App!</h1>
-            </div>
-        );
-    }
+
+export default function App() {
+
+  useEffect(() => {
+
+    select("#colhead")
+      .selectAll("tr")
+      .data([0])
+      .enter()
+      .append("tr")
+      .each(function(d) {
+        select(this)
+          .selectAll("th")
+          .data(row)
+          .enter()
+          .append("th")
+          .text(r => r)
+        })
+
+    select("#tabular")
+      .selectAll("tr")
+      .data(arr)
+      .enter()
+      .append("tr")
+      .each(function(d) {
+        select(this)
+          .selectAll("td")
+          .data(row)
+          .enter()
+          .append("td")
+          .text(r => '_' + r )
+      })
+  },[])
+
+  return (
+    <div id='app'>
+      <div id='banner'>
+        <span id='title'>ylikothek</span>
+        <span id='subtitle'>MATERIALS DATABASE</span>
+      </div>
+      <div id='viewpane'>
+        <table id='colhead'></table>
+        <div id='tablebox'>
+          <table id='tabular'></table>
+        </div>
+      </div>
+    </div>
+  )
 }
-
-export default App;

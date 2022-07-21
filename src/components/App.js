c8bf9bbacb5c6c15754f9f0e50bbc6f4c2105483
damoncrockett <damoncrockett@hotmail.com>
2022-07-21 14:50:05 -0400

    working filters

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,29 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, { Component, useState, useEffect, useRef, createRef } from 'react';
 import { data } from './data';
 
-let sortedData;
 const cols = ['INSTITUTION', 'CATALOG NO.', 'FULL BARCODE', 'COMMON NAME',
               'SAMPLE TYPE', 'ACQUISITION DATE', 'GEOGRAPHIC ORIGIN',
               'MANUFACTURER', 'AVAILABLE DATA', 'COLOR'];
 
-function TableCells({ sortCol, asc }) {
+function TableCells({ sortCol, asc, filterList }) {
+
+  let processedData = [...data];
+
+  filterList.forEach((item, i) => {
+    if ( item !== '' ) {
+      processedData = processedData.filter(d => d[i].toLowerCase().includes(item.toLowerCase()))
+    }
+  });
 
   if ( asc === true ) {
-    sortedData = data.sort((a,b) => a[cols.indexOf(sortCol)].localeCompare(b[cols.indexOf(sortCol)]));
+    processedData = processedData.sort((a,b) => a[cols.indexOf(sortCol)].localeCompare(b[cols.indexOf(sortCol)]));
   } else if ( asc === false ) {
-    sortedData = data.sort((a,b) => b[cols.indexOf(sortCol)].localeCompare(a[cols.indexOf(sortCol)]));
+    processedData = processedData.sort((a,b) => b[cols.indexOf(sortCol)].localeCompare(a[cols.indexOf(sortCol)]));
   }
 
   return (
     <tbody id='tbody'>
-      {sortedData.map(d => <tr className='datarow'>{d.map(i => <td>{i}</td>)}</tr>)}
+      {processedData.map(d => <tr className='datarow'>{d.map(i => <td>{i}</td>)}</tr>)}
     </tbody>
   )
 }
@@ -25,7 +32,8 @@ export default function App() {
 
   const [sortCol, setSortCol] = useState('INSTITUTION');
   const [asc, setAsc] = useState(true);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerms, setSearchTerms] = useState(['','','','','','','','','','']);
+  const fieldsRef = useRef(cols.map(() => createRef()));
 
   return (
     <div id='app'>
@@ -37,7 +45,7 @@ export default function App() {
         <table id='tabular'>
           <thead id='thead'>
             <tr>
-              {cols.map(c => <th className="searchField"><form onSubmit={e => {e.preventDefault();console.log('submit')}}><input type="text" value={searchTerm} onChange={e => setSearchTerm(e.target.value)} className="searchField" /></form></th>)}
+              {cols.map((c,i) => <th className="searchField"><form onSubmit={e => {e.preventDefault();const tmp=[...searchTerms];tmp[i]=fieldsRef.current[i].current.value;setSearchTerms(tmp)}}><input ref={fieldsRef.current[i]} type="text" className="searchField" /></form></th>)}
             </tr>
             <tr>
               {cols.map(c => <th><button className={sortCol === c && asc === true ? 'material-icons active' : 'material-icons'} onClick={() => {setSortCol(c);setAsc(true)}} >arrow_upward</button><button title='sort' className={sortCol === c && asc === false ? 'material-icons active' : 'material-icons'} onClick={() => {setSortCol(c);setAsc(false)}} >arrow_downward</button></th>)}
@@ -46,7 +54,7 @@ export default function App() {
               {cols.map(c => <th className='colLabel'>{c}</th>)}
             </tr>
           </thead>
-          <TableCells sortCol={sortCol} asc={asc}/>
+          <TableCells sortCol={sortCol} asc={asc} filterList={searchTerms}/>
         </table>
       </div>
     </div>

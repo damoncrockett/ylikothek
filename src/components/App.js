68e3cbabfbec343112f08a27f7aabb67889a3480
damoncrockett <damoncrockett@hotmail.com>
2022-07-31 23:04:05 -0400

    webfont loader

--- src/components/App.js
+++ src/components/App.js
@@ -188,12 +188,7 @@ export default function App() {
 
   const [clickedId,setClickedId] = useState(null);
 
-  const [loading, setLoading] = useState(true);
   const [tablePage, setTablePage] = useState(1);
-
-  setTimeout(() => {
-    setLoading(false);
-  }, 10000)
 
   useEffect(() => {
     const tmp = [];
@@ -250,7 +245,6 @@ export default function App() {
 
   return (
     <div id='app'>
-      {loading && <div id='loadingScreen'><div id='loadingMsg'><p id='loadingTitle'>IOMA</p><p id='loadingSubtitle'>Index Of Materials</p></div><div className='loader'></div></div>}
       <div id='scrollers'>
         <button id='top' className='material-icons' onClick={() => document.getElementById("viewpane").scrollTo(0,0)}>vertical_align_top</button>
         <button id='uup' className='material-icons' onClick={() => document.getElementById("viewpane").scrollTo(0,document.getElementById("viewpane").scrollTop - document.getElementById("tabular").getBoundingClientRect().height / 10)}>keyboard_double_arrow_up</button>
